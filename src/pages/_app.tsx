53c3ee00ba17ac962aec0411d253a60c2fb92fef
Amaru333 <amrutesharun0599@gmail.com>
2025-01-12 21:14:33 +0530

    Added speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { ThemeProvider } from "@/context/ThemeContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <ThemeProvider>
         <div className="bg-slate-50 dark:bg-slate-950 text-slate-950 dark:text-slate-50">
           <Component {...pageProps} />
