a013967c420382eac356f2f5bee43934de356bf0
Amaru333 <amrutesharun0599@gmail.com>
2025-03-26 10:45:38 -0400

    Removed ;

--- src/pages/projects/workquest/index.tsx
+++ src/pages/projects/workquest/index.tsx
@@ -9,7 +9,7 @@ function WorkQuestPage() {
         <title>Amrutesh Arun | WorkQuest</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <WorkQuest />;
+      <WorkQuest />
     </>
   );
 }
