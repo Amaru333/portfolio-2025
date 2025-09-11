7fc144d7c3f773ecd967cf511dd412f1603e7ace
Amaru333 <amrutesharun0599@gmail.com>
2025-09-11 19:52:14 -0400

    navbar color changes

--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -1,24 +1,24 @@
-"use client";
+'use client';
 
-import { useTheme } from "../context/ThemeContext";
-import { Moon, Sun, Home, Briefcase, Mail, User } from "lucide-react";
-import { motion } from "motion/react";
-import Link from "next/link";
+import { useTheme } from '../context/ThemeContext';
+import { Moon, Sun, Home, Briefcase, Mail, User } from 'lucide-react';
+import { motion } from 'motion/react';
+import Link from 'next/link';
 
 export default function Navbar() {
   const { theme, toggleTheme } = useTheme();
 
   const navItems = [
-    { icon: Home, label: "Home", href: "/" },
-    { icon: Briefcase, label: "Projects", href: "/#projects" },
-    { icon: Mail, label: "Contact", href: "/#contact" },
-    { icon: User, label: "About", href: "/#about" },
+    { icon: Home, label: 'Home', href: '/' },
+    { icon: Briefcase, label: 'Projects', href: '/#projects' },
+    { icon: Mail, label: 'Contact', href: '/#contact' },
+    { icon: User, label: 'About', href: '/#about' },
   ];
 
   return (
     <nav className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-30">
       <motion.div
-        className="flex items-center bg-background/80 backdrop-blur-sm rounded-full shadow-lg p-2 text-black dark:text-white"
+        className="flex items-center bg-white/40 dark:bg-black/40 backdrop-blur-sm rounded-full shadow-lg p-2 text-black dark:text-white"
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.3 }}
@@ -42,7 +42,7 @@ export default function Navbar() {
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.95 }}
         >
-          {theme === "light" ? <Moon size={24} /> : <Sun size={24} />}
+          {theme === 'light' ? <Moon size={24} /> : <Sun size={24} />}
         </motion.button>
       </motion.div>
     </nav>
