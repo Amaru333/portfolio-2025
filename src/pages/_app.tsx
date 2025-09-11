880c2325346214382380c05f8779d5afe8f94cf9
Amaru333 <amrutesharun0599@gmail.com>
2025-09-11 19:45:11 -0400

    Added padding

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import Navbar from "@/common/Navbar";
-import { ThemeProvider } from "@/context/ThemeContext";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import Navbar from '@/common/Navbar';
+import { ThemeProvider } from '@/context/ThemeContext';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Analytics />
       <SpeedInsights />
       <ThemeProvider>
-        <div className="bg-slate-50 dark:bg-slate-950 text-slate-950 dark:text-slate-50">
+        <div className="bg-slate-50 dark:bg-slate-950 text-slate-950 dark:text-slate-50 px-4 md:px-0">
           <Component {...pageProps} />
           <Navbar />
         </div>
